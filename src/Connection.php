a8a0c72ec706c1caf8b863ec5e59afe21c02ec6a
MrChuckNovice <teedji77@hotmail.fr>
2021-02-01 16:51:51 +0100

    commit 01/02/2021

--- src/Connection.php
+++ src/Connection.php
@@ -9,4 +9,56 @@ class Connection extends PDO {
         return new PDO('mysql:host=localhost;dbname=arcade;charset=utf8','root','', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     }
+
+    public static function create(string $table, array $data) {
+
+        $bdd = self::getPDO();
+
+        //On veux construire la requete sous la forme INSERT INTO $table($data.keys) VALUES(?)
+        $req = "INSERT INTO" . $table;
+        $req .= " ('".implode("','", array_keys($data))."')";
+        $req .= " VALUES (:".implode(", :", array_keys($data)).") ";
+        $response = $bdd->prepare($req);
+        $response->execute($data);
+        return $bdd->lastInsertId();
+    }
+
+    public static function delete(string $table, array $data) {
+
+        $bdd = self::getPDO();
+
+        $req = "DELETE FROM " . $table . "WHERE " . array_keys($data)[0] . "= :" . array_keys($data)[0];
+
+        $response = $bdd->prepare($req);
+
+        $response->execute($data);
+
+        return;
+    }
+
+    public static function update(string $table, array $data, string $idField = null ) {
+        
+        $bdd = self::getPDO();
+        
+        $req = "UPDATE " . $table . " SET ";
+
+        $whereIdString = '';
+
+        $whereIdString = ($idField = null) ? "WHERE '" . $idField . "' = :" . $idField : " WHERE id = :id";
+
+        foreach($data as $key => $value) {
+            if ($key !== 'id') {
+                $req .= "'" . "' = :" . $key . ", ";
+            }
+
+        }
+
+        $req = substr($req, 0, -2);
+        $req .= $whereIdString;
+
+        $response = $bdd->prepare($req);
+
+        $response->execute($data);
+        return $bdd->lastInsertId();
+    }
 }
--- src/Models/annonce.php
+++ src/Models/annonce.php
@@ -1,9 +1,14 @@
 <?php
 namespace App\Models;
+
+use App\Connection;
 use App\Helpers\Text;
 use DateTime;
+use Exception;
 
-class Annonce{
+class Annonce extends Connection{
+
+    const TABLE_NAME = "annonce";
 
     private $id;
 
@@ -16,6 +21,8 @@ class Annonce{
     private $created_at;
 
     private $categorie;
+
+    private $photo;
     
     public function getTitre(): ?string 
     {
@@ -47,5 +54,74 @@ class Annonce{
     public function getID(): ?int
     {
         return $this->id;
+    }
+
+    public function getPhoto() 
+    {
+        return $this->getPhoto;
+    }
+
+    public function setPhoto($photo) {
+
+        if(isset($photo) && $photo['error'] == 0){
+            // Testons si le fichier n'est pas trop gros
+            if($photo['size']<= 10000000) {
+                //l'extension est autorisée
+                $infosfichier = pathinfo($photo['name']);
+                $extension_upload = $infosfichier['extension'];
+                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+                if (in_array($extension_upload, $extensions_autorisees)) {
+
+                    $this->photo=$photo;
+                    return $this;
+                }
+            }else{
+                throw new Exception('photo trop grande');
+            }
+        }else{
+            throw new Exception("une erreur est survenue à l'upload du fichier");
+        }
+    }
+
+    public function savePhoto() 
+    {
+        $photo = $this->getPhoto();
+
+        $extension = pathinfo($photo['name'])['extension'];
+
+        $newName = "Annonce_" . $this->getID();
+
+        $newNameWithExtension = $newName . "." . $extension;
+
+        move_uploaded_file($photo['tmp_name'], './public/uploads/' . $newNameWithExtension);
+
+        $data = [
+            'id' => $this->getID(),
+            'photo' => $newNameWithExtension
+        ];
+
+        Connection::update(self::TABLE_NAME, $data);
+
+        return $this;
+    }
+
+    private function createMiniature() {
+
+        $photo = $this->getPhoto();
+    
+        $extension = pathinfo($photo['name'])['extension'];
+        $oldName = "annonce_" . $this->getId();
+        $oldNameWithExtension = $oldName . "." . $extension;
+
+        $titreAncienneImage = $oldNameWithExtension;
+        $extension = $extension;
+        $dossierEnregistrement = './public/uploads';
+        $titreNouvelleImage = $titreAncienneImage . '_300x300.' . $extension;
+        $resultMiniature = createMiniature($titreAncienneImage, $extension, $dossierEnregistrement, $titreNouvelleImage);
+        if(!$resultMiniature){
+            echo "Il y a eu un problème lors de la création de la miniature.";
+            return;
+        }
+
     }
 }
